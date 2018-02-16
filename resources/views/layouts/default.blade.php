ce34f93b2276d96478b84d0d72995a768fdfc0de
yanxia <yanxia0414@gmail.com>
2018-02-16 02:46:12 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Sample App') --Construted By Laravel</title>
+    <title>@yield('title', 'Sample App') --Emily's Website Construted By Laravel</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+    @include('layouts._header')
+    <div class="container">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
   </body>
 </html>
