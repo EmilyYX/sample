c87412c864bc8837f23cd6dc2cf9ff91ea43afa4
yanxia <yanxia0414@gmail.com>
2018-03-07 09:09:05 +0000

    20180307

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -104,4 +104,11 @@ class UsersController extends Controller
         session()->flash('success', '恭喜你，激活成功！');
         return redirect()->route('users.show', [$user]);
     }
+
+    public function show(User $user){
+        $statuses=$user->statuses()
+                        ->orderBy('created_at', 'desc')
+                        ->paginate(30);
+        return view('users.show', compact('user', 'statuses'));
+    }
 }
